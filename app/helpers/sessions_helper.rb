ace99dbca286ca5dd049aadc41c43e3e532e9182
Will Sheehan <sheehan5@me.com>
2016-07-03 17:22:21 -1000

    Finish user edit, update, index, and destroy action

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -38,4 +38,20 @@ module SessionsHelper
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  #redirects to the stored location (or to the default)
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  #stores the url trying to be accessed
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+
 end
